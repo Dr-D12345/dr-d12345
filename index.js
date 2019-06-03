450389d99da08cb9d445c790aa5d2d544a3ba6ae
Dr_D00M <derekjonp@gmail.com>
2019-06-02 21:16:02 -0400

    organized file structure for github pages

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 
 var app = express();
 app.get('/', function (req, res) {
-    res.sendFile('public/index.html', { root: __dirname })
+    res.sendFile('index.html', { root: __dirname })
 });
-app.use(express.static(__dirname + "/public", {
+app.use(express.static(__dirname + "/", {
     immutable: true
 }));
 
